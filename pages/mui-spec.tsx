956723b2437b9668f4711bfb0e401e60abcd88a4
Samuel_Roellin <samfromaway@gmail.com>
2022-06-02 13:39:26 +0200

    spec page

---
+++ pages/mui-spec.tsx
@@ -0,0 +1,34 @@
+import type { NextPage } from 'next';
+import { Box, Grid } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const StyledInput = styled(Box)(() => ({
+  backgroundColor: 'green',
+}));
+
+const sxStyle = {
+  backgroundColor: 'red',
+};
+
+const inlineStyle = {
+  backgroundColor: 'blue',
+};
+
+const MuiSpecificityPage: NextPage = () => {
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <StyledInput
+          height="300px"
+          width="300px"
+          bgcolor="orange"
+          placeholder="styled"
+          sx={sxStyle}
+          style={inlineStyle}
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default MuiSpecificityPage;
