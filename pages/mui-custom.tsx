0789b6776566ef72edadcf0e182446e2c9f3bf06
Samuel_Roellin <samfromaway@gmail.com>
2022-06-28 09:54:23 +0200

    fix add comments

--- pages/mui-custom.tsx
+++ pages/mui-custom.tsx
@@ -2,6 +2,36 @@ import type { NextPage } from 'next';
 import { Grid, InputBase, Theme } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
+// docs: https://mui.com/system/basics/#the-sx-prop
+
+export const StyledBox = styled(Grid)({
+  // root selector (.MuiBox-root)
+  background: 'red',
+
+  '&': {
+    // '&' points to the root selector which is the same as the above (.MuiBox-root)
+    background: 'red',
+  },
+  '&&': {
+    // also a root selector but with higher CSS specificity (.MuiBox-root.MuiBox-root)
+    background: 'red',
+  },
+  '& .ChildSelector': {
+    // child selector (.MuiBox-root .ChildSelector)
+    background: 'orange',
+
+    // this '&' points to the current selector which is '.MuiBox-root .ChildSelector'
+    '& .NestedChildSelector': {
+      // nested child selector (.MuiBox-root .ChildSelector .NestedChildSelector) (#1)
+      background: 'yellow',
+    },
+  },
+  '& .ChildSelector .NestedChildSelector': {
+    // same as (#1) (.MuiBox-root .ChildSelector .NestedChildSelector)
+    background: 'yellow',
+  },
+});
+
 const StyledInput = styled(InputBase)(({ theme }) => ({
   color: 'blue',
   'label + &': {
