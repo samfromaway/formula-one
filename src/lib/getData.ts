c1ad79e0b8268f4139cff9ad2857085ea8264dc5
Sam <samfromaway@gmail.com>
2023-03-12 14:55:00 +0100

    fix 2023

--- src/lib/getData.ts
+++ src/lib/getData.ts
@@ -1,7 +1,5 @@
 import { mainUrl } from './dbConnection';
 
-const USE_DB = true;
-
 type GetDataResponse<T> = { data: T | null; errors: string | null };
 
 const makeParamsString = (params?: any) => {
@@ -26,35 +24,25 @@ export default async function getData<T>(
     url += `?${paramsString}`;
   }
 
-  if (USE_DB) {
-    try {
-      const res = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'x-rapidapi-host': 'api-formula-1.p.rapidapi.com',
-          'x-rapidapi-key': '0f2fb9a6e43206a0f37c4b67ad022417',
-        },
-      });
-      const data = await res.json();
+  try {
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'api-formula-1.p.rapidapi.com',
+        'x-rapidapi-key': '0f2fb9a6e43206a0f37c4b67ad022417',
+      },
+    });
+    const data = await res.json();
 
-      if (!!data.errors && data.errors.length !== 0) {
-        return { data: null, errors: data.errors };
-      }
-      if (!data || !data.response) {
-        return { data: null, errors: 'no data returned' };
-      }
-
-      return { data: data.response, errors: null };
-    } catch (e) {
-      return { data: null, errors: e as string };
+    if (!!data.errors && data.errors.length !== 0) {
+      return { data: null, errors: data.errors };
     }
+    if (!data || !data.response) {
+      return { data: null, errors: 'no data returned' };
+    }
+
+    return { data: data.response, errors: null };
+  } catch (e) {
+    return { data: null, errors: e as string };
   }
-
-  const myPromise: Promise<GetDataResponse<T>> = new Promise(
-    (resolve, reject) => {
-      resolve({ data: fakeData, errors: null });
-    }
-  );
-
-  return myPromise;
 }
