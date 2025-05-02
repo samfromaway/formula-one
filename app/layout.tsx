191964ef3033cf20f86feb621935f8f2cf34718e
samfromaway <samfromaway@gmail.com>
2025-05-02 07:21:29 +0200

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,20 @@
 'use client';
 
 import Head from 'next/head';
-import { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
-import { CacheProvider, EmotionCache } from '@emotion/react';
 import { darkTheme, lightTheme } from '@/styles/theme';
-import createEmotionCache from '@/styles/createEmotionCache';
 import { useMemo } from 'react';
 import { Layout as LayoutComponent } from '@/components/layout';
 import '@fontsource/titillium-web/600.css';
 import '@fontsource/titillium-web/700.css';
 import useDarkMode from '@/utils/useDarkMode';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
-
-interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
-}
-
-export default function Layout(props: MyAppProps) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
 
   const currentTheme = useMemo(
@@ -30,15 +23,22 @@ export default function Layout(props: MyAppProps) {
   );
 
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={currentTheme}>
-        <LayoutComponent toggleDarkMode={toggleDarkMode}>
-          <Component {...pageProps} />
-        </LayoutComponent>
-      </ThemeProvider>
-    </CacheProvider>
+    <html lang="en">
+      <body>
+        <AppRouterCacheProvider>
+          <Head>
+            <meta
+              name="viewport"
+              content="initial-scale=1, width=device-width"
+            />
+          </Head>
+          <ThemeProvider theme={currentTheme}>
+            <LayoutComponent toggleDarkMode={toggleDarkMode}>
+              {children}
+            </LayoutComponent>
+          </ThemeProvider>
+        </AppRouterCacheProvider>{' '}
+      </body>
+    </html>
   );
 }
